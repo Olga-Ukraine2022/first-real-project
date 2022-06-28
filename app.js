2ab0f02a257c57782f4a951bb503b03cc5453a65
Olga-Ukraine2022 <nikolenko79@gmail.com>
2022-06-28 14:56:36 +0200

    Integrate API, actualize searching by the city

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-function changeDegrees(event) {
-  event.preventDefault();
-  let degrees = document.querySelector("#change-table");
-  let temperature = degrees.innerHTML;
-  degrees.innerHTML = Math.round((temperature * 9) / 5 + 32);
-}
-let farenhaitLink = document.querySelector("#change-farenhaits");
-farenhaitLink.addEventListener("click", changeDegrees);
-
-function changeDegreesBack(event) {
-  event.preventDefault();
-  let degrees = document.querySelector("#change-table");
-  degrees.innerHTML = "25";
-}
-
-let celsiumLink = document.querySelector("#change-celsium");
-celsiumLink.addEventListener("click", changeDegreesBack);
-
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#example-City");
-  let header = document.querySelector("#current-city");
-  header.innerHTML = `${searchInput.value}`;
-}
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
-
 function formatDate(date) {
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let months = [
@@ -55,7 +28,52 @@ function formatDate(date) {
   }
   return `${currentDate} ${currentMonth}, ${currentDay} ${currentHours}:${currentMinutes}`;
 }
+function displayWeatherCondition(response) {
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#change-table").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#higest").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#lowest").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+function search(city) {
+  let apiKey = "f523b5f66e6722e873ea21469d0b366f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#example-city").value;
+  search(city);
+}
+function changeDegrees(event) {
+  event.preventDefault();
+  let degrees = document.querySelector("#change-table");
+  let temperature = degrees.innerHTML;
+  degrees.innerHTML = Math.round((temperature * 9) / 5 + 32);
+}
+function changeDegreesBack(event) {
+  event.preventDefault();
+  let degrees = document.querySelector("#change-table");
+  degrees.innerHTML = "25";
+}
+
 let h3 = document.querySelector("h3");
 let currentTime = new Date();
-
 h3.innerHTML = formatDate(currentTime);
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let farenhaitLink = document.querySelector("#change-farenhaits");
+farenhaitLink.addEventListener("click", changeDegrees);
+let celsiumLink = document.querySelector("#change-celsium");
+celsiumLink.addEventListener("click", changeDegreesBack);
+
+search("New York");
